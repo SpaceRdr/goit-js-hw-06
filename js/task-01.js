3896063685b852b695b847ed03c706ae9c74a30f
SpaceRider <spacerider.ua@gmail.com>
2023-07-02 17:45:25 +0300

    Update task-01.js

--- js/task-01.js
+++ js/task-01.js
@@ -1,5 +1,6 @@
 const navRoot = document.querySelector('#categories');
 console.log(navRoot);
 const navCategory = document.querySelector('.item');
-console.dir(navCategory);
+// console.dir(navCategory);
+console.log(navCategory.h2);
 // element.querySelectorAll(selector);  .textContent
