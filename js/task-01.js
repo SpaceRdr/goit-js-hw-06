58bfadfe4375267c149eaa674778bbb235797a83
SpaceRider <spacerider.ua@gmail.com>
2023-07-02 17:25:39 +0300

    edit simp

--- js/task-01.js
+++ js/task-01.js
@@ -1,5 +1,5 @@
 const navRoot = document.querySelector('#categories');
 console.log(navRoot);
-const navCategory = document.querySelector('li');
+const navCategory = document.querySelector('.item');
 console.log(navCategory);
 // element.querySelectorAll(selector); 
