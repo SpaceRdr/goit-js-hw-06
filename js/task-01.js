bb28d9f140b3ce23dd08c6bcd973e87b84b49282
SpaceRider <spacerider.ua@gmail.com>
2023-07-02 20:38:27 +0300

    Update task-01.js

--- js/task-01.js
+++ js/task-01.js
@@ -4,8 +4,8 @@ console.log(navRoot.childNodes.length);
 const navCategory = document.querySelector('.item');
 const elemH2 = navCategory.firstChild;
 console.log(elemH2.nodeType);
-const elemList = navCategory.firstChild.nextSibling;
-console.log(elemList);
+// const elemList = navCategory.firstChild.nextSibling;
+// console.log(elemList);
 
 // console.dir(navCategory);
 // console.log(navCategory.innerText);
