c7df202f2d4feb302bcb8ebe01407b0a4364448c
SpaceRider <spacerider.ua@gmail.com>
2023-07-03 22:03:37 +0300

    Update task-01.js

--- js/task-01.js
+++ js/task-01.js
@@ -1,28 +1,31 @@
 const navRoot = document.querySelector("#categories");
 // console.log(navRoot);
 
-console.log(navRoot.childNodes);
+//console.log(navRoot.childNodes);
 // let numCatRoot = 0;
 // for (let index = 0; index < navRoot.childNodes.length; index++) {
 //   if (navRoot.childNodes[index].nodeName !== "#text") numCatRoot++;
 // }
  
 const navCategRoot = navRoot.getElementsByClassName('item');
-console.log("Number of categories: ", navCategRoot);
+//console.log("Number of categories: ", navCategRoot);
 console.log("Number of categories: ", 
-   navRoot.getElementsByClassName('item').length);
+   navRoot.getElementsByClassName('item').length, '\n');
 // console.log("Number of categories: ", numCatRoot);
 
-const navCategory = document.querySelector(".item");
+// const navCategory = document.querySelector(".item");
+navCategRoot.forEach(el => {
+   console.log(el);
+});
 //console.dir(navCategory.childNodes);
 //console.log(navCategory.childNodes);
 console.log("Num ind: ", navCategRoot.length);
 for (let ind = 0; ind < navCategRoot.length; ind++) {
-   console.log("It's categories: ", navCategRoot[ind]);
-   console.log("children SubCat: ", 
+   //console.log("It's categories: ", navCategRoot[ind]);
+   console.log("Category: ", 
       navCategRoot[ind].children[0].innerText);
-   console.log("children num Nodes: ", 
-      navCategRoot[ind].children[1].children.length);
+   console.log("Elements: ", 
+      navCategRoot[ind].children[1].children.length, '\n');
    // let nameSubCateg = navCategRoot[ind].getElementsByTagName('h2');
    // console.log("First Child: ", nameSubCateg);
    // console.log("First Child: ", nameSubCateg.textContent);
@@ -30,9 +33,7 @@ for (let ind = 0; ind < navCategRoot.length; ind++) {
    // console.log("Child Nodes: ", nodesSubCayeg);
 
 }
-// navCategRoot.forEach(el => {
-//    console.log(el);
-// });
+
 
    // console.log("Category: ", navCategory.childNodes[1].textContent);
    //console.log(navCategory.childNodes[3]);
