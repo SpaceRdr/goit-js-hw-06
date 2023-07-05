661bbbabf91a2da932639650d792a07b5737eb23
SpaceRider <spacerider.ua@gmail.com>
2023-07-05 18:28:55 +0300

    Update task-02.js

--- js/task-02.js
+++ js/task-02.js
@@ -12,9 +12,12 @@ console.log(insItem);
 const editList = document.querySelector("#ingredients");
 console.log(editList);
 
-insItem[0] = document.createElement("li");
-console.log(insItem[0]);
-insItem[0].textContent = ingredients[0];
+ingredients.forEach(function(elem, ind) {  
+  insItem[ind] = document.createElement("li");
+  console.log(insItem[ind]);
+  insItem[ind].textContent = elem;
+  // ingredients[ind]
+  editList.append(insItem[ind]);
 
-editList.append(insItem[0]);
+});
 console.log(editList);
