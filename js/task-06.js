fa144e04c098d05cae5fe7367c551101b1ab468c
SpaceRider <spacerider.ua@gmail.com>
2023-07-06 22:51:51 +0300

    Update task-06.js

---
+++ js/task-06.js
@@ -0,0 +1,7 @@
+const txtInput = document.querySelector("#validation-input");
+const txtInputLength = document.querySelector('[data-length="6"]');
+console.log(txtInput);
+console.log(txtInputtLength);
+
+// txtInput.addEventListener('blur', validatorInp);
+// console.log();
