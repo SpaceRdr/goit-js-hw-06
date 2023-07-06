930b0035cf918958fd263e0cd1f8dca2d6c31a68
SpaceRider <spacerider.ua@gmail.com>
2023-07-07 01:49:08 +0300

    Update task-06.js

--- js/task-06.js
+++ js/task-06.js
@@ -1,9 +1,14 @@
 const txtInput = document.querySelector("#validation-input");
 // const txtInputLength = document.querySelector('[data-length="6"]');
 console.log(txtInput);
+console.dir(txtInput);
 
 const txtInputLength = txtInput.getAttribute("data-length");
 console.log(txtInputLength);
+
+// function validatorInp () {
+ 
+// }
 
 // txtInput.addEventListener('blur', validatorInp);
 // console.log();
