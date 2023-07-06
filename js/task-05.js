146ea0dc83c9e78e71588268259cacbbf4f2bd21
SpaceRider <spacerider.ua@gmail.com>
2023-07-06 21:37:54 +0300

    Update task-05.js

--- js/task-05.js
+++ js/task-05.js
@@ -1,11 +1,11 @@
 const txtInput = document.querySelector('name-input');
 const txtOutput = document.querySelector('name-output');
 
-txtInput.addEventListener("input", hendInput);
-
-function hendInput() {
+const hendInput = () => {
    if (txtInput.value === "")
       txtOutput.textContent = "Anonymous";
       else
       txtOutput.textContent = txtInput.value;
 }
+
+txtInput.addEventListener("input", hendInput);
