aae2fa82d5617059e09062d585fbbaab0be37375
SpaceRider <spacerider.ua@gmail.com>
2023-07-07 19:01:33 +0300

    Update task-06.js

--- js/task-06.js
+++ js/task-06.js
@@ -1,26 +1,28 @@
 const txtInput = document.querySelector("#validation-input");
 // const txtInputLength = document.querySelector('[data-length="6"]');
-console.log(txtInput);
-console.dir(txtInput);
+// console.log(txtInput);
+// console.dir(txtInput);
 
 const txtInputLength = Number(txtInput.getAttribute("data-length"));
-console.log(txtInputLength);
-console.log('txtInputLength', typeof txtInputLength);
+// console.log(txtInputLength);
+// console.log('txtInputLength', typeof txtInputLength);
 
 function validatorInp () {
-   console.log('Length ', txtInput.value.length);
-   console.log('txtInput.value.length ', typeof txtInput.value.length);
+  //  console.log('Length ', txtInput.value.length);
+  //  console.log('txtInput.value.length ', typeof txtInput.value.length);
  if (txtInput.value.length !== txtInputLength)
-   {console.log('Error 5');
+   {
+    // console.log('Error 5');
    txtInput.classList.remove("valid");
    txtInput.classList.add("invalid");
-   // txtInput.id = 'validation-input.invalid';
+   
    }
    else
-   {console.log('Right 5');
+   {
+    // console.log('Right 5');
    txtInput.classList.remove("invalid");
    txtInput.classList.add("valid");
-   // txtInput.id = 'validation-input.valid';
+  
    }
 }
 
