8e3daeb8ab09ab6c13524d7d41a1969b33ff7391
SpaceRider <spacerider.ua@gmail.com>
2023-07-07 21:37:24 +0300

    Update task-09.js

--- js/task-09.js
+++ js/task-09.js
@@ -3,3 +3,20 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const body = document.querySelector('body');
+console.dir(body);
+const ColorBtn = document.querySelector('.change-color')
+const nameColor = document.querySelector('.color')
+
+
+function ChngColorBtn() {
+  const newColor = getRandomHexColor();
+  console.log('newColor ', newColor);
+  body.style.backgroundColor = newColor;
+  nameColor.textContent = newColor;
+}
+
+ColorBtn.addEventListener('click', ChngColorBtn);
+
+
