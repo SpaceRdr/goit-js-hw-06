3943615e9fc3fb98e77dcb082b243ab89639bf6c
SpaceRider <spacerider.ua@gmail.com>
2023-07-07 20:48:39 +0300

    Update task-08.js

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,21 @@
 
+const form = document.querySelector(".login-form");
+
+function manualInpSubmit(event) {
+  event.preventDefault();
+  const {
+    elements: { login, password }
+  } = event.currentTarg;
+  console.log('event ', event);
+
+  if (login.value === "" || password.value === "") 
+      {
+         alert("Please fill in all the fields!");
+         return console.log("Please fill in all the fields!");
+      }
+  console.log(`Login: ${login.value}, Password: 
+                     ${password.value}`);
+  event.currentTarg.reset();
+};
+
+form.addEventListener("submit", manualInpSubmit);
