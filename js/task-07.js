e97218d525e5cbf70b95952d7fc2df955fa2ee09
SpaceRider <spacerider.ua@gmail.com>
2023-07-08 15:40:43 +0300

    Update task-07.js

--- js/task-07.js
+++ js/task-07.js
@@ -1,9 +1,6 @@
 const inptElem = document.getElementById("font-size-control");
-console.log('inptElem ', inptElem);
-console.dir('inptElem ', inptElem);
+
 const txtElem = document.getElementById("text");
-console.log('txtElem ', txtElem);
-console.dir('txtElem ', txtElem);
 
 inptElem.addEventListener("input", function () {
   txtElem.style.fontSize = inptElem.value + "px";
