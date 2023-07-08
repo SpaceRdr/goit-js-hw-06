89af9e159ae9b8cd4178471cd1232c1c8c62d46a
SpaceRider <spacerider.ua@gmail.com>
2023-07-08 15:38:25 +0300

    Update task-06.js

--- js/task-06.js
+++ js/task-06.js
@@ -1,30 +1,15 @@
 const txtInput = document.querySelector("#validation-input");
-// const txtInputLength = document.querySelector('[data-length="6"]');
-// console.log(txtInput);
-// console.dir(txtInput);
 
 const txtInputLength = Number(txtInput.getAttribute("data-length"));
-// console.log(txtInputLength);
-// console.log('txtInputLength', typeof txtInputLength);
 
-function validatorInp () {
-  //  console.log('Length ', txtInput.value.length);
-  //  console.log('txtInput.value.length ', typeof txtInput.value.length);
- if (txtInput.value.length !== txtInputLength)
-   {
-    // console.log('Error 5');
-   txtInput.classList.remove("valid");
-   txtInput.classList.add("invalid");
-   
-   }
-   else
-   {
-    // console.log('Right 5');
-   txtInput.classList.remove("invalid");
-   txtInput.classList.add("valid");
-  
-   }
+function validatorInp() {
+  if (txtInput.value.length !== txtInputLength) {
+    txtInput.classList.remove("valid");
+    txtInput.classList.add("invalid");
+  } else {
+    txtInput.classList.remove("invalid");
+    txtInput.classList.add("valid");
+  }
 }
 
-txtInput.addEventListener('blur', validatorInp);
-// console.log();
+txtInput.addEventListener("blur", validatorInp);
