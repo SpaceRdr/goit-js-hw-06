9f7da545d5550aade279bbcfe2bf8b51a2a5b77e
SpaceRider <spacerider.ua@gmail.com>
2023-07-08 15:01:46 +0300

    Update task-01.js

--- js/task-01.js
+++ js/task-01.js
@@ -1,54 +1,14 @@
-const navRoot = document.querySelector("#categories");
-console.log(navRoot);
- 
-const navCategRoot = navRoot.getElementsByClassName('item');
-//console.log("Number of categories: ", navCategRoot);
-console.log("Number of categories: ", 
-   navRoot.getElementsByClassName('item').length, '\n');
-   
-// console.log("Number of categories: ", numCatRoot);
-//console.dir(navCategory.childNodes);
-//console.log(navCategory.childNodes);
-//console.log("Num ind: ", navCategRoot.length);
-for (let ind = 0; ind < navCategRoot.length; ind++) {
-   //console.log("It's categories: ", navCategRoot[ind]);
-   console.log("Category: ", 
-      navCategRoot[ind].children[0].innerText);
-   console.log("Elements: ", 
-      navCategRoot[ind].children[1].children.length, '\n');
-}
 
 
-// //console.log(navRoot.childNodes);
-// // let numCatRoot = 0;
-// // for (let index = 0; index < navRoot.childNodes.length; index++) {
-// //   if (navRoot.childNodes[index].nodeName !== "#text") numCatRoot++;
-// // }
- 
-// const navCategRoot = navRoot.getElementsByClassName('item');
-// //console.log("Number of categories: ", navCategRoot);
-// console.log("Number of categories: ", 
-//    navRoot.getElementsByClassName('item').length, '\n');
-   
-// // console.log("Number of categories: ", numCatRoot);
+const catgrItem = document.querySelectorAll("#categories > li.item");
+console.log(catgrItem);
 
-// // const navCategory = document.querySelector(".item");
-// // navCategRoot.forEach(el => {
-// //    console.log(el);
-// // });
-// //console.dir(navCategory.childNodes);
-// //console.log(navCategory.childNodes);
-// //console.log("Num ind: ", navCategRoot.length);
-// for (let ind = 0; ind < navCategRoot.length; ind++) {
-//    //console.log("It's categories: ", navCategRoot[ind]);
-//    console.log("Category: ", 
-//       navCategRoot[ind].children[0].innerText);
-//    console.log("Elements: ", 
-//       navCategRoot[ind].children[1].children.length, '\n');
-//    // let nameSubCateg = navCategRoot[ind].getElementsByTagName('h2');
-//    // console.log("First Child: ", nameSubCateg);
-//    // console.log("First Child: ", nameSubCateg.textContent);
-//    // let nodesSubCayeg = navCategRoot[ind].getElementsByTagName('ul');
-//    // console.log("Child Nodes: ", nodesSubCayeg);
+const itemsCount = catgrItem.length;
+console.log(`Number of categories: ${ itemCount } `);
 
-// }
+catgrItem.forEach(category => {
+  const catName = category.querySelector("h2").textContent;
+  const catElements = category.querySelectorAll("li").length;
+  console.log(`Category: ${catName}`);
+  console.log(`Elements: ${catElements}`);
+});
